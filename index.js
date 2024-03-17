67b41d0c98eafd259cfd4c3f94ea1febb8ba143a
rahultheeda <91866801+rahultheeda@users.noreply.github.com>
2024-03-17 12:39:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+var randomnum1 = Math.floor(Math.random()*6)+1; //0-6
+var randomImg = "dice" + randomnum1 + ".png";
+var image1src = "images/" + randomImg;
+var img1 = document.querySelectorAll("img")[0];
+img1.setAttribute('src', image1src);
+
+var randomnum2 = Math.floor(Math.random()*6)+1; //0-6
+var randomImg = "dice" + randomnum2 + ".png";
+var image2src = "images/" + randomImg;
+var img2 = document.querySelectorAll("img")[1];
+img2.setAttribute('src', image2src);
+
+if(randomnum1>randomnum2){
+  document.querySelector('h1').innerHTML="Player 1 won";
+}
+else if(randomnum1<randomnum2){
+  document.querySelector('h1').innerHTML="Player 2 won";
+}
+else{
+    document.querySelector('h1').innerHTML="Draw!1";
+}
